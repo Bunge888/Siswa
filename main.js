035a5588ebb0d1709611fd55a262c988f7069b40
bunge888 <burhandoang59@gmail.com>
2024-08-12 10:29:49 +0700

    menambah konfigurasi firebase

---
+++ main.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCbqYHEkbK8rlmTEvT7IRxamrueBVOvf2o",
+  authDomain: "insan-cemerlang-25a53.firebaseapp.com",
+  projectId: "insan-cemerlang-25a53",
+  storageBucket: "insan-cemerlang-25a53.appspot.com",
+  messagingSenderId: "383282706725",
+  appId: "1:383282706725:web:bf9fd56cea37cb3a0fc2cc",
+  measurementId: "G-31EQF6PDVN"
+};
